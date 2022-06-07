1da6d96c750ea3cfb52a2b96b466c94ecec118e6
joomladagennl <joomladagennl@com-linssh024.srv.combell-ops.net>
2022-06-07 12:10:16 +0200

    :heavy_plus_sign: add NL lang files and clean template

---
+++ public_html/templates/clean/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	<title>JoomlaDagen Nederland</title>
+	<meta name="description" content="Hét jaarlijkse event voor iedereen die websites bouwt met Joomla.">
+	<meta name="robots" content="index, follow">
+	<meta name="author" content="Stichting Joomla Nederland">
+	<meta property="og:title" content="JoomlaDagen Nederland ">
+	<meta property="og:site_name" content="JoomlaDagen">
+	<meta property="og:url" content="https://www.joomladagen.nl">
+	<meta property="og:description" content="Hét jaarlijkse event voor iedereen die websites bouwt met Joomla.">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="https://www.joomladagen.nl/images/joomladagen.jpg">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@joomladagen">
+	<meta name="twitter:creator" content="@joomladagen">
+	<link rel="stylesheet" href="//joomladagen.nl/templates/clean/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,600">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway:400,800">
+	<style>
+		body, html {
+			height: 100%;
+		}
+		.bg {
+			background-image: url(images/pattern.png);
+			background-position: center;
+			background-repeat: repeat;
+			background-size: 35%;
+			background-attachment: fixed;
+		}
+		.jumbotron {
+			background: #f9fafc;
+			padding-bottom: 1em;
+			margin: 0;
+			max-width: 750px;
+		}
+		h1 {
+			font-family: 'Raleway', sans-serif;
+			font-weight: 800;
+			color: #25304f;
+			font-size: 2.4em;
+		}
+		p.lead {
+			font-size: 1.1em;
+			font-weight: 300;
+		}
+		p {
+			font-family: 'Roboto', sans-serif;
+			font-size: 1.05em;
+			font-weight: 300;
+		}
+		h3 {
+			font-family: 'Raleway', sans-serif;
+			font-size: 1.5em;
+			font-weight: 400;
+			color: #25304f;
+		}
+		strong {
+			font-weight: 600;
+		}
+	</style>
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start':
+					new Date().getTime(), event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NQZ8SP');
+	</script>
+</head>
+<body class="bg">
+<noscript>
+	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQZ8SP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<div class="container-fluid h-100">
+	<div class="row justify-content-center h-100">
+		<div class="col-sm-11 col-md-10 col-lg-9 col-xl-6 my-auto pt-3 pb-3">
+			<div class="jumbotron text-center mx-auto">
+				<h1>JoomlaDagen Nederland</h1>
+				<p class="lead">
+					Gezien de huidige omstandigheden en onzekerheden rond het coronavirus heeft
+					<a href="https://www.stichtingjoomlanederland.nl/">Stichting Joomla Nederland</a> besloten de JoomlaDagen uit te stellen totdat de situatie dit weer toelaat.
+				</p>
+				<p>
+					Een nieuwe datum zal worden geprikt zodra er voldoende zekerheid is voor doorgang van ons - normaal gesproken - jaarlijkse evenement.
+				</p>
+				<br>
+			</div>
+			<div class="col-sm-11 mx-auto" style="font-size:10px; color: #fff; text-align: center; margin-top:10px;">JoomlaDay™ events are officially recognized and licensed by, but not organized or operated by, Open Source Matters, Inc. (OSM) on behalf of The Joomla! Project™. Each JoomlaDay is managed independently by a local community. Use of the Joomla!® name, symbol, logo, JoomlaDay,™ and JDay™ and related trademarks is licensed by Open Source Matters, Inc.</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
